338f4a5fc80a58d1d0dfbeb9ffc1a96b7202024c
HoCG <rhwkd5012@naver.com>
2022-01-10 14:09:17 +0900

    로그인창 형성, 알림창 형성

---
+++ src/components/DocumentInfoSideBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <v-img :src="this.$store.state.admin.currentUser.profileImage"></v-img>
+        <button class="documentShowBtn">내 문서 리스트</button>
+        <button class="documentShowBtn">제출받은 문서 리스트</button>
+    </div>
+</template>
+<script>
+    export default {
+
+    }
+</script>
+<style>
+    .documentShowBtn {
+        width: 100%;
+        text-align: center;
+    }
+</style>
